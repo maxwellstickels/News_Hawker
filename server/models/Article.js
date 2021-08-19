1c7cd41daecbc7e3b5e54514fa2dff1fe450ba32
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-08-18 19:05:07 -0700

    made a subschema for source in articles model

--- server/models/Article.js
+++ server/models/Article.js
@@ -2,7 +2,14 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const articleSchema = new Schema({
-  source: {},
+  source: {
+    id: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+  },
   author: [
     {
       type: String,
