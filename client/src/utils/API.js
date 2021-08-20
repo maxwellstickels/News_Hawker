de328a77a4245ffb5db8ef9fb897fa38d712db25
Nikola <88djony@gmail.com>
2021-08-19 17:22:07 -0700

    connecting and testing API's

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,10 +3,22 @@ import axios from 'axios';
 const BASEURL = process.env.REACT_APP_BASEURL;
 const APIKEY = process.env.REACT_APP_APIKEY;
 
+console.log(BASEURL, APIKEY)
 
-export default {
-    search(query) {
-        return axios.get(`${BASEURL}${query}${APIKEY}&rating=pg`);
-    },
-};
 
+async function search () {
+    // console.log(query)
+    // let responseJson = await axios.get(`${BASEURL}${query}${APIKEY}`, 
+    fetch('https://gnews.io/api/v4/search?q=example&token=7f1fd78c002cebd14e04533b292de6bb')
+    // fetch('https://newsapi.org/v2/everything?q=keyword&apiKey=5f2820da9b6340da87e0692275c16702')
+    
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+}
+
+
+export { search }
