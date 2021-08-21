f8216bf6771e513c44da9ee443ec136dfa9a4b3e
Nikola <88djony@gmail.com>
2021-08-21 14:04:41 -0700

    added SavedArticles

--- client/src/components/SavedArticles.js
+++ client/src/components/SavedArticles.js
@@ -1,11 +1,9 @@
 
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
 import Auth from '../utils/auth';
-
 import { REMOVE_ARTICLE } from '../utils/mutations';
 import { removeNewsId } from '../utils/localStorage';
 
